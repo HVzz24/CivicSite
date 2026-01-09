59ee1ba1464d6ac158535fbc550774baae24ad45
HVzz24 <hvengenece@gmail.com>
2026-01-09 22:21:18 +0700

    fix the postition admin page

--- application/views/administrator/menu-admin.php
+++ application/views/administrator/menu-admin.php
@@ -22,27 +22,27 @@
               <?php
                 $cek=$this->model_app->umenu_akses("identitaswebsite",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/identitaswebsite'><i class='fa fa-circle-o'></i> Identitas Website</a></li>";
+                  echo "<li><a href='".base_url()."administrator/identitaswebsite'><i class='fa fa-plus-square'></i> Identitas Website</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("menuwebsite",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/menuwebsite'><i class='fa fa-circle-o'></i> Menu Website</a></li>";
+                  echo "<li><a href='".base_url()."administrator/menuwebsite'><i class='fa fa-plus-square'></i> Menu Website</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("halamanbaru",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/halamanbaru'><i class='fa fa-circle-o'></i> Halaman Baru</a></li>";
+                  echo "<li><a href='".base_url()."administrator/halamanbaru'><i class='fa fa-plus-square'></i> Halaman Baru</a></li>";
                 }
 				
 				$cek=$this->model_app->umenu_akses("alamat",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/alamat'><i class='fa fa-circle-o'></i> Alamat Kontak</a></li>";
+                  echo "<li><a href='".base_url()."administrator/alamat'><i class='fa fa-plus-square'></i> Alamat Kontak</a></li>";
                 }
 				
 				$cek=$this->model_app->umenu_akses("kepalainstansi",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/kepalainstansi'><i class='fa fa-circle-o'></i> Kepala Instansi</a></li>";
+                  echo "<li><a href='".base_url()."administrator/kepalainstansi'><i class='fa fa-plus-square'></i> Kepala Instansi</a></li>";
                 }
               ?>
               </ul>
@@ -53,37 +53,37 @@
               <?php
                 $cek=$this->model_app->umenu_akses("listberita",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/listberita'><i class='fa fa-circle-o'></i> Berita</a></li>";
+                  echo "<li><a href='".base_url()."administrator/listberita'><i class='fa fa-plus-square'></i> Berita</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("kategoriberita",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/kategoriberita'><i class='fa fa-circle-o'></i> Kategori Berita</a></li>";
+                  echo "<li><a href='".base_url()."administrator/kategoriberita'><i class='fa fa-plus-square'></i> Kategori Berita</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("tagberita",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/tagberita'><i class='fa fa-circle-o'></i> Tag Berita</a></li>";
+                  echo "<li><a href='".base_url()."administrator/tagberita'><i class='fa fa-plus-square'></i> Tag Berita</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("komentarberita",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/komentarberita'><i class='fa fa-circle-o'></i> Komentar Berita</a></li>";
+                  echo "<li><a href='".base_url()."administrator/komentarberita'><i class='fa fa-plus-square'></i> Komentar Berita</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("sensorkomentar",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/sensorkomentar'><i class='fa fa-circle-o'></i> Sensor Komentar</a></li>";
+                  echo "<li><a href='".base_url()."administrator/sensorkomentar'><i class='fa fa-plus-square'></i> Sensor Komentar</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("album",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/album'><i class='fa fa-circle-o'></i> Berita Foto</a></li>";
+                  echo "<li><a href='".base_url()."administrator/album'><i class='fa fa-plus-square'></i> Berita Foto</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("gallery",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/gallery'><i class='fa fa-circle-o'></i> Gallery Berita Foto</a></li>";
+                  echo "<li><a href='".base_url()."administrator/gallery'><i class='fa fa-plus-square'></i> Gallery Berita Foto</a></li>";
                 }
               ?>
               </ul>
@@ -95,22 +95,22 @@
               <?php
                 $cek=$this->model_app->umenu_akses("playlist",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/playlist'><i class='fa fa-circle-o'></i> Playlist Video</a></li>";
+                  echo "<li><a href='".base_url()."administrator/playlist'><i class='fa fa-plus-square'></i> Playlist Video</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("video",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/video'><i class='fa fa-circle-o'></i> Video</a></li>";
+                  echo "<li><a href='".base_url()."administrator/video'><i class='fa fa-plus-square'></i> Video</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("tagvideo",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/tagvideo'><i class='fa fa-circle-o'></i> Tag Video</a></li>";
+                  echo "<li><a href='".base_url()."administrator/tagvideo'><i class='fa fa-plus-square'></i> Tag Video</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("komentarvideo",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/komentarvideo'><i class='fa fa-circle-o'></i> Komentar Video</a></li>";
+                  echo "<li><a href='".base_url()."administrator/komentarvideo'><i class='fa fa-plus-square'></i> Komentar Video</a></li>";
                 }
               ?>
               </ul>
@@ -122,22 +122,22 @@
               <?php
                 $cek=$this->model_app->umenu_akses("iklanatas",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/iklanatas'><i class='fa fa-circle-o'></i> Iklan Atas</a></li>";
+                  echo "<li><a href='".base_url()."administrator/iklanatas'><i class='fa fa-plus-square'></i> Iklan Atas</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("iklanhome",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/iklanhome'><i class='fa fa-circle-o'></i> Iklan Home</a></li>";
+                  echo "<li><a href='".base_url()."administrator/iklanhome'><i class='fa fa-plus-square'></i> Iklan Home</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("iklansidebar",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/iklansidebar'><i class='fa fa-circle-o'></i> Iklan Sidebar</a></li>";
+                  echo "<li><a href='".base_url()."administrator/iklansidebar'><i class='fa fa-plus-square'></i> Iklan Sidebar</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("banner",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/banner'><i class='fa fa-circle-o'></i> Link Terkait</a></li>";
+                  echo "<li><a href='".base_url()."administrator/banner'><i class='fa fa-plus-square'></i> Link Terkait</a></li>";
                 }
               ?>
               </ul>
@@ -149,17 +149,17 @@
               <?php
                 $cek=$this->model_app->umenu_akses("logowebsite",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/logowebsite'><i class='fa fa-circle-o'></i> Logo Website</a></li>";
+                  echo "<li><a href='".base_url()."administrator/logowebsite'><i class='fa fa-plus-square'></i> Logo Website</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("templatewebsite",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/templatewebsite'><i class='fa fa-circle-o'></i> Template Website</a></li>";
+                  echo "<li><a href='".base_url()."administrator/templatewebsite'><i class='fa fa-plus-square'></i> Template Website</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("background",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/background'><i class='fa fa-circle-o'></i> Background Website</a></li>";
+                  echo "<li><a href='".base_url()."administrator/background'><i class='fa fa-plus-square'></i> Background Website</a></li>";
                 }
               ?>
               </ul>
@@ -171,32 +171,32 @@
               <?php
                 $cek=$this->model_app->umenu_akses("agenda",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/agenda'><i class='fa fa-circle-o'></i> Agenda</a></li>";
+                  echo "<li><a href='".base_url()."administrator/agenda'><i class='fa fa-plus-square'></i> Agenda</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("sekilasinfo",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/sekilasinfo'><i class='fa fa-circle-o'></i> Sekilas Info</a></li>";
+                  echo "<li><a href='".base_url()."administrator/sekilasinfo'><i class='fa fa-plus-square'></i> Sekilas Info</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("jajakpendapat",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/jajakpendapat'><i class='fa fa-circle-o'></i> Jajak Pendapat</a></li>";
+                  echo "<li><a href='".base_url()."administrator/jajakpendapat'><i class='fa fa-plus-square'></i> Jajak Pendapat</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("ym",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/ym'><i class='fa fa-circle-o'></i> Yahoo Messanger</a></li>";
+                  echo "<li><a href='".base_url()."administrator/ym'><i class='fa fa-plus-square'></i> Yahoo Messanger</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("download",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/download'><i class='fa fa-circle-o'></i> Download Area</a></li>";
+                  echo "<li><a href='".base_url()."administrator/download'><i class='fa fa-plus-square'></i> Download Area</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("pesanmasuk",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/pesanmasuk'><i class='fa fa-circle-o'></i> Pesan Masuk</a></li>";
+                  echo "<li><a href='".base_url()."administrator/pesanmasuk'><i class='fa fa-plus-square'></i> Pesan Masuk</a></li>";
                 }
               ?>
               </ul>
@@ -208,12 +208,12 @@
               <?php
                 $cek=$this->model_app->umenu_akses("manajemenuser",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/manajemenuser'><i class='fa fa-circle-o'></i> Manajemen User</a></li>";
+                  echo "<li><a href='".base_url()."administrator/manajemenuser'></i> Manajemen User</a></li>";
                 }
 
                 $cek=$this->model_app->umenu_akses("manajemenmodul",$this->session->id_session);
                 if($cek==1 OR $this->session->level=='admin'){
-                  echo "<li><a href='".base_url()."administrator/manajemenmodul'><i class='fa fa-circle-o'></i> Manajemen Modul</a></li>";
+                  echo "<li><a href='".base_url()."administrator/manajemenmodul'></i> Manajemen Modul</a></li>";
                 }
               ?>
               </ul>
